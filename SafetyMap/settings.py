00e0122ec80f55675f38ccf1b35b1630ca48afbb
KussymYerke <e_kusym@kbtu.kz>
2024-06-28 00:13:41 +0500

    Updated Allowed Hosts

--- SafetyMap/settings.py
+++ SafetyMap/settings.py
@@ -124,3 +124,4 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'SafetyMap/static')]
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+ALLOWED_HOSTS = ['188.127.36.29']
